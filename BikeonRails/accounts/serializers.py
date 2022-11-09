1db824ffd69bed9beab38166959088cbebf7dfdf
Marck <marvin.roses@gmail.com>
2022-11-08 22:35:16 -0500

    FEAT: register, email verification

---
+++ BikeonRails/accounts/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+
+from accounts.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type'}, min_length=4, max_length=12, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class EmailVerificationSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(max_length=555)
+
+    class Meta:
+        model = User
+        fields = ['token']
+
+
+class ResetPasswordEmailRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField(min_length=2)
+
+    class Meta:
+        fields = ['email']
--- BikeonRails/accounts/views.py
+++ BikeonRails/accounts/views.py
@@ -1,3 +1,67 @@
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.shortcuts import render
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import smart_str, smart_bytes, DjangoUnicodeDecodeError
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from accounts.models import User
+from accounts.serializers import RegisterSerializer, EmailVerificationSerializer, ResetPasswordEmailRequestSerializer
+from accounts.utils import Util
+
+from django.urls import reverse
+from django.conf import settings
+
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
+import jwt
+
+
+class RegisterView(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = User.objects.get(email=user_data['email'])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relativeLink = reverse('email-verify')
+        absurl = 'http://' + current_site + relativeLink + "?token=" + str(token)
+        email_body = f'Hi  {user.username} Use the link below to verify your email \n {absurl}'
+        data = {'email_body': email_body, 'to_email': user.email, 'email_subject': 'Verify your email'}
+
+        Util.send_email(data)
+
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(APIView):
+    serializer_class = EmailVerificationSerializer
+
+    token_param_config = openapi.Parameter(
+        'token', in_=openapi.IN_QUERY, description='Description', type=openapi.TYPE_STRING)
+
+    @swagger_auto_schema(manual_parameters=[token_param_config])
+    def get(self, request):
+        token = request.GET.get('token')
+        try:
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            user = User.objects.get(id=payload['user_id'])
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            return Response({'email': 'Successfully activated'}, status=status.HTTP_200_OK)
+        except jwt.ExpiredSignatureError as identifier:
+            return Response({'error': 'Activation Expired'}, status=status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as identifier:
+            return Response({'error': 'Invalid token'}, status=status.HTTP_400_BAD_REQUEST)
