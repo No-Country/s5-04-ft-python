5ca45b5627ec66bb83f5b58c6d6580ab57cc237c
Francisco Rey <reyf1998@outlook.es>
2022-11-14 15:56:16 -0300

    FEAT: eslint and prettier configuration

--- front/src/components/Test.jsx
+++ front/src/components/Test.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
 const Test = () => {
-  return <div>Test rama</div>;
-};
+    return <div>Test rama</div>
+}
 
-export default Test;
+export default Test
