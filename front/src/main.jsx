4c562f7e7f3b6903e022dc8425258add38c011b7
Daniel Albanez <danielalbanez40@gmail.com>
2022-11-15 12:11:16 -0300

    FEAT: Adding login view

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import Theme from "./theme/Theme";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Theme from './theme/Theme'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Theme>
-    <App />
-  </Theme>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+        <Theme>
+            <App />
+        </Theme>
+)
--- front/src/router/Router.jsx
+++ front/src/router/Router.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Home from "../pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Login from "../auth/Login";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
-  );
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+      </Routes>
+  )
 };
 
 export default Router;
