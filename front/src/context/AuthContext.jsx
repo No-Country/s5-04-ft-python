f33cacfc82a34ccffbbebcb3a80240955afa0516
Francisco Rey <reyf1998@outlook.es>
2022-11-16 18:28:53 -0300

    FEAT: add alert and changes in router

--- front/src/context/AuthContext.jsx
+++ front/src/context/AuthContext.jsx
@@ -1,11 +1,15 @@
 import axios from 'axios'
+import { Toast } from '../utils/swalToast'
 import { createContext, useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const API_ROUTE = import.meta.env.VITE_APP_API_ROUTE
 
 export const AuthContext = createContext({})
 
 export const AuthProvider = ({ children }) => {
+    const navigate = useNavigate()
+
     const [dataAuth, setDataAuth] = useState({})
     console.log(dataAuth)
 
@@ -36,7 +40,13 @@ export const AuthProvider = ({ children }) => {
             }
             await fetch(`${API_ROUTE}/auth/register/`, requestOptions)
                 .then((response) => response.json())
-                .then((data) => console.log(data))
+                .then((data) => setDataAuth(data))
+
+            Toast.fire({
+                icon: 'success',
+                title: `Cuenta creada correctamente`,
+            })
+            navigate('/registerSuccessfull')
         } catch (error) {
             console.log(error)
         }
