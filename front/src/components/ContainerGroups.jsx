892d2a98d6bd647cf499720464fb43224c1e31cb
Francisco Rey <reyf1998@outlook.es>
2022-11-24 15:23:07 -0300

    FIX: fix error in console

--- front/src/components/ContainerGroups.jsx
+++ front/src/components/ContainerGroups.jsx
@@ -10,7 +10,6 @@ const ContainerGroups = () => {
             gridTemplateColumns="1fr 1fr 1fr"
             width="100%"
             item
-            spacing={2}
             padding={3}
             gap="2rem"
         >
