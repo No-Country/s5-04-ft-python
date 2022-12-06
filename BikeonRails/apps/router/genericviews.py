a936f6f1de1315fcbcfe4aa4eaf8a502f20cfdce
Marck <marvin.roses@gmail.com>
2022-12-05 20:22:18 -0500

    FIX: view fiel

--- BikeonRails/apps/router/genericviews.py
+++ BikeonRails/apps/router/genericviews.py
@@ -18,16 +18,6 @@ class UserRouteViewset(ModelViewSet):
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     print(serializer)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     headers = self.get_success_headers(serializer.data)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-#
-#     # permission_classes = [IsAuthenticatedOrReadOnly]
-#
     def create(self, request, *args, **kwargs):
         data = {
             "name_route": request.data['name_route'],
