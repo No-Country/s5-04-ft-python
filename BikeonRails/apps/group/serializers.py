7ce66ba4375814ddaceda7e59b61eb8d74a2f784
Marck <marvin.roses@gmail.com>
2022-12-07 20:17:56 -0500

    FEAT: list all description of group and users

--- BikeonRails/apps/group/serializers.py
+++ BikeonRails/apps/group/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
+
+from apps.accounts.models import User
 from apps.group.models import (BikeGroupsModel, EventModel, GroupUsersRolModel)
+
+
+class UserAllSeralizer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'image']
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -22,5 +31,33 @@ class BikeGroupsAddSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BikeGroupsModel
-        fields = ["user_member_id"]
+        fields = ["id"]
 
+
+class GroupUsersRolSerilizer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GroupUsersRolModel
+        fields = ['group_id']
+
+    def create(self, validated_data):
+
+        return GroupUsersRolModel.objects.create(**validated_data)
+
+
+class BikeGroupsAll(serializers.ModelSerializer):
+    user_member_id = UserAllSeralizer(many=True)
+
+    class Meta:
+        model = BikeGroupsModel
+        fields = ['user_member_id', 'name_group', 'image', 'city', 'event']
+
+
+class GroupUserAllSeralizer(serializers.ModelSerializer):
+
+    user_id = UserAllSeralizer(read_only=True)
+    group_id = BikeGroupsAll(read_only=True)
+
+    class Meta:
+        model = GroupUsersRolModel
+        fields = ['id', 'user_id', 'rol', 'group_id']
